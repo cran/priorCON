6cbe8bbf93a911d9a36ec61bf1b33543ad7f1592
Christos Adam <econp266@econ.soc.uoc.gr>
2024-08-19 08:10:02 +0000

    version 0.1.0

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# priorCON 0.1.0
+
+## Major changes
+
+- Initial package version.
---
+++ inst/doc/Introduction.R
@@ -0,0 +1,24 @@
+## ----Figure1, eval=TRUE, echo=FALSE, out.width = '100%', fig.align = "center"----
+knitr::include_graphics("priorCON_flowchart.png")
+
+## ----Figure2, eval=TRUE, echo=FALSE, out.width = '100%', fig.align = "center"----
+knitr::include_graphics("priorCON_fig2.png")
+
+## ----Figure3, eval=TRUE, echo=FALSE, out.width = '100%', fig.align = "center"----
+knitr::include_graphics("priorCON_fig3.png")
+
+## ----Figure4, eval=TRUE, echo=FALSE, out.width = '100%', fig.align = "center"----
+knitr::include_graphics("priorCON_fig4.png")
+
+## ----Costraster, eval=TRUE, echo=FALSE, out.width = '70%', fig.align = "center"----
+knitr::include_graphics("cost_raster.png")
+
+## ----Features, eval=TRUE, echo=FALSE, out.width = '70%', fig.align = "center"----
+knitr::include_graphics("features.png")
+
+## ----Basic, eval=TRUE, echo=FALSE, out.width = '70%', fig.align = "center"----
+knitr::include_graphics("basic_output.png")
+
+## ----Connectivity, eval=TRUE, echo=FALSE, out.width = '70%', fig.align = "center"----
+knitr::include_graphics("connectivity_output.png")
+
